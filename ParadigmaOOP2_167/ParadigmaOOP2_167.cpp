70640256d2f53e2412c286ab64bf5b8577f2ddc1
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-05-29 00:48:51 -0700

    class orang

--- ParadigmaOOP2_167/ParadigmaOOP2_167.cpp
+++ ParadigmaOOP2_167/ParadigmaOOP2_167.cpp
@@ -1,1 +1,15 @@
-#include
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
